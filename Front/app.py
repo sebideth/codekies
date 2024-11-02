7364d2e647a4d839aff0fdf7e05b2b11189ac985
Malena Mena <mcmena@fi.uba.ar>
2024-11-02 19:38:45 -0300

    Creación de base.html y adaptación de templates en Flask (id #21)

---
+++ Front/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/work-single')
+def workSingle():
+    return render_template('work-single.html')
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
