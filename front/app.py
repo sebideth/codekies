a02bdeee6d6dc5b3f47c9adeb8fc622b1a115a0f
FrancoDArias <fdarias@fi.uba.ar>
2024-11-05 12:01:01 -0300

    Creacion de upload_pet.html y de publicaciones.html, con avances en el formulario de upload_pet y ambas rutas para los templates creados. #26

--- front/app.py
+++ front/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -29,6 +29,28 @@ def work():
 @app.route('/auth')
 def auth():
     return render_template('auth.html')
+
+@app.route('publicaciones')
+def publicaciones():
+    return render_template('publicaciones.html')
+
+@app.route('/upload_pet', methods=["GET","POST"])
+def upload_pet(logeado):
+    if request.method == "POST":
+        #definir logeado
+        if logeado:
+            animal = request.form.get('animal')
+            raza = request.form.get('raza')
+            condicion = request.form.get('condicion')
+            color = request.form.get('color')
+            descripcion = request.form.get('descripcion')
+            fecha = request.form.get('fecha')
+            foto = request.files.get('foto')
+            resuelto = request.form.get('resuelto')
+            return redirect(url_for('publicaciones.html'))
+        else:
+            return(redirect(url_for('auth.html')))
+    return render_template('upload_pet.html')
 
 if __name__ == '__main__':
     app.run("127.0.0.1", port="5000", debug=True)
