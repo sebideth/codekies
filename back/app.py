dfcddc0a06823ef25155e8792815fef1f438e1e5
Diego Nadares <dnadares@fi.uba.ar>
2024-11-09 10:54:23 -0300

    Add command to create database. Also add gitignore. #13

--- back/app.py
+++ back/app.py
@@ -1,6 +1,57 @@
-from flask import Flask, render_template
+import click
+from flask import Flask
+from colorama import init
+from colorama import Fore, Style
+from sqlalchemy import text
+from db import engine
+
 
 app = Flask(__name__)
+
+
+@app.cli.command("init-database", help="Initialize the database.")
+@click.argument("host", default="localhost")
+@click.argument("username", default="root")
+@click.argument("password", default="change_me")
+@click.argument("database", default="codekis")
+def init_database(host, username, password, database):
+    print(f"[{Fore.GREEN}*{Style.RESET_ALL}] Initializing database...")
+    print(f"[{Fore.GREEN}*{Style.RESET_ALL}] Username -> {username}")
+    print(f"[{Fore.GREEN}*{Style.RESET_ALL}] Password -> {password}")
+    print(f"[{Fore.GREEN}*{Style.RESET_ALL}] Host -> {host}")
+    try:
+        connection = engine.connect()
+        connection.execute(text("DROP DATABASE codekis"))
+        connection.execute(text("CREATE DATABASE codekis"))
+        connection.execute(text(f"USE {database}"))
+        connection.execute(text("CREATE TABLE users ("
+                                "id INT AUTO_INCREMENT PRIMARY KEY,"
+                                "nombreUsuario VARCHAR(100) NOT NULL UNIQUE,"
+                                "password VARCHAR(255) NOT NULL,"
+                                "nombre VARCHAR(255),"
+                                "apellido VARCHAR(255),"
+                                "email VARCHAR(255) UNIQUE"
+                                "telefono VARCHAR(20))"
+                                )
+                           )
+        connection.execute(text("CREATE TABLE animales ("
+                                "id INT AUTO_INCREMENT PRIMARY KEY,"
+                                "animal VARCHAR(255) NOT NULL,"
+                                "raza VARCHAR(255) NOT NULL,"
+                                "condicion VARCHAR(255) NOT NULL,"
+                                "color VARCHAR(50),"
+                                "ubicacion VARCHAR(255)"
+                                ",urlFoto VARCHAR(255),"
+                                " descripcion TEXT,"
+                                "fecha DATETIME NOT NULL,"
+                                "resuelto BOOLEAN DEFAULT FALSE,"
+                                "userID INT,"
+                                "FOREIGN KEY (userID) REFERENCES users(id))"
+                                )
+                           )
+    except Exception as e:
+        print(e)
+
 
 if __name__ == "__main__":
     app.run("127.0.0.1", port="5001", debug=True)
