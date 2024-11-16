9a1eb98926614fc9c471d2e68dcd57e354307674
Diego Nadares <dnadares@fi.uba.ar>
2024-11-16 11:18:24 -0300

    Add exception at connect

--- back/app.py
+++ back/app.py
@@ -29,13 +29,22 @@ def register():
     for key in validate_fields:
         if key not in user_data:
             return jsonify({'error': f'Keys obligatorias {validate_fields}'}), http.client.BAD_REQUEST
-    with engine.connect() as connection:
-        try:
-            connection.execute(INSERT_USER, user_data)
-            connection.commit()
-        except exc.IntegrityError as error:
-            logger.error(f"Could not create user {user_data.get('username')}. {error}")
-            return jsonify({"error": "Usuario o email existentes"}), http.client.BAD_REQUEST
+
+    try:
+        connection = engine.connect()
+    except exc.DatabaseError as error:
+        logger.error(f"No pudimos conectarnos a la base de datos. {error}")
+        return jsonify({'error': 'Hubo un error al obtener tus datos :('}), http.client.INTERNAL_SERVER_ERROR
+
+    try:
+        connection.execute(INSERT_USER, user_data)
+        connection.commit()
+    except exc.IntegrityError as error:
+        logger.error(f"Could not create user {user_data.get('username')}. {error}")
+        return jsonify({"error": "Usuario o email existentes"}), http.client.BAD_REQUEST
+    finally:
+        connection.close()
+
     return jsonify({"message": "Usuario creado correctamente"}), http.client.CREATED
 
 
@@ -51,22 +60,30 @@ def login():
         if key not in user_data:
             return jsonify({'error': f'Keys obligatorias {validate_fields}'}), http.client.BAD_REQUEST
 
-    with engine.connect() as connection:
-        try:
-            user = connection.execute(LOGIN_USER_QUERY, user_data).fetchone()
-            if not user:
-                return jsonify({'error': 'Usuario o Password invalidos'}), http.client.FORBIDDEN
-        except Exception as error:
-            logger.error(f"Could not fetch user {user_data.get('username')}. {error}")
-            return jsonify({"error": "Usuario o email existentes"}), http.client.INTERNAL_SERVER_ERROR
-    session["logged_in"] = True
+    try:
+        connection = engine.connect()
+    except exc.DatabaseError as error:
+        logger.error(f"No pudimos conectarnos a la base de datos. {error}")
+        return jsonify({'error': 'Hubo un error al obtener tus datos :('}), http.client.INTERNAL_SERVER_ERROR
+
+    try:
+        user = connection.execute(LOGIN_USER_QUERY, user_data).fetchone()
+        if not user:
+            return jsonify({'error': 'Usuario o Password invalidos'}), http.client.FORBIDDEN
+        session["logged_in"] = True
+        session["user_id"] = user.id
+    except Exception as error:
+        logger.error(f"Could not fetch user {user_data.get('username')}. {error}")
+        return jsonify({"error": "Usuario o email existentes"}), http.client.INTERNAL_SERVER_ERROR
+    finally:
+        connection.close()
     return jsonify({"message": "Login exitoso"}), http.client.OK
 
 
 @app.route("/logout", methods=["GET"])
 def logout():
     session.clear()
-    return jsonify({"message": "Bye"}), http.client.NOT_MODIFIED
+    return jsonify({"message": "Bye"}), http.client.OK
 
 
 if __name__ == "__main__":
